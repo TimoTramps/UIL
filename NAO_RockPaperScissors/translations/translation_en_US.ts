ee2935a5d9c37022c2fef4a11ba3e47b3b2d8b12
Timotej Trampuz <timotej.trampuz@tscng.org>
2025-05-30 08:49:15 +0200

    Add files via upload

---
+++ NAO_RockPaperScissors/translations/translation_en_US.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/NAO plays Rock Paper Scissors/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Thank you for playing</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Thank you for playing</translation>
+        </message>
+    </context>
+</TS>
